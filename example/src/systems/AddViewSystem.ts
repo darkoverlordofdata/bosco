ff0dcf2ae8a6c3b7bdd3094a5508317264a8398d
darkoverlordofdata <darkoverlordofdata@gmail.com>
2015-11-04 19:45:29 -0800

    daily

--- example/src/systems/AddViewSystem.ts
+++ example/src/systems/AddViewSystem.ts
@@ -14,17 +14,12 @@ module example {
   import IReactiveSystem = entitas.IReactiveSystem;
   import IInitializeSystem = entitas.IInitializeSystem;
 
-  export class AddViewSystem implements IInitializeSystem, IReactiveSystem {
+  export class AddViewSystem implements IReactiveSystem {
 
 
     public get trigger():TriggerOnEvent {
       return Matcher.Resource.onEntityAdded();
     }
-
-    public initialize() {
-    }
-
-
 
     /**
      * Execute when a Resource is added
@@ -38,52 +33,6 @@ module example {
         viewContainer.addChild(s);
         e.addView(s);
       }
-    }
-  }
-
-
-  /**
-   * Builds a prefab composite sprite
-   *
-   * @param config
-   * @returns {PIXI.Sprite}
-   */
-  function prefab(name:string, parent=viewContainer): PIXI.Sprite {
-    var s = _prefab(bosco.config.resources[name]);
-    console.log(name, bosco.config.resources[name], s);
-    if (parent) parent.addChild(s);
-    return s;
-  }
-
-  function _prefab(config): PIXI.Sprite {
-    if (Array.isArray(config)) {
-      var container = new PIXI.Sprite();
-      for (var i=0, l=config.length; i<l; i++) {
-        container.addChild(_prefab(config[i]));
-      }
-      return container;
-    } else {
-      var sprite = new PIXI.Sprite(Texture.fromFrame(config.path));
-      for (var k in config) {
-        switch(k) {
-          case 'anchor':
-            sprite.anchor.set(config.anchor.x, config.anchor.y);
-            break;
-          case 'scale':
-            sprite.scale.set(config.scale.x, config.scale.y);
-            break;
-          case 'position':
-            sprite.position.set(config.position.x, config.position.y);
-            break;
-          case 'rotation':
-            sprite.rotation = config.rotation.z;
-            break;
-          case 'tint':
-            sprite.tint = config.tint;
-            break;
-        }
-      }
-      return sprite;
     }
   }
 }
